28ddb82b4c01cd875e7282964c372988d7e0b897
bema1212 <bouwmanenergie@outlook.com>
2024-07-09 09:36:56 +0200

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -2,15 +2,17 @@ const puppeteer = require('puppeteer');
 
 module.exports = async (req, res) => {
   try {
+    // Launch a headless browser
     const browser = await puppeteer.launch({
       args: ['--no-sandbox', '--disable-setuid-sandbox']
     });
     const page = await browser.newPage();
 
+    // Visit the specified URL
     await page.goto('https://www.wozwaardeloket.nl');
 
     // Wait for the button to be available and then click it
-    await page.waitForSelector('#kaart-bekijken-btn'); // Replace 'button-selector' with the actual selector of the button
+    await page.waitForSelector('#kaart-bekijken-btn'); // Selector for the button with ID 'kaart-bekijken-btn'
     await page.click('#kaart-bekijken-btn');
 
     // Wait for the page to load completely after clicking
@@ -19,12 +21,13 @@ module.exports = async (req, res) => {
     // Get the HTML content of the page
     const html = await page.content();
 
+    // Close the browser
     await browser.close();
 
     // Return the HTML content
     res.status(200).send(html);
   } catch (error) {
-    console.error(error);
+    console.error('Error during Puppeteer operation:', error);
     res.status(500).send('Something went wrong');
   }
 };
